54d393fb0866f1ec17cf652e246b3fa398dd8f18
Bjørn Tore Svinningen <postmannen@gmail.com>
2020-06-16 06:33:10 +0200

    moved files into its own repo

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/raalabs/mixwebserver
+
+go 1.14
+
+require (
+	github.com/aliyun/aliyun-oss-go-sdk v0.0.0-20190307165228-86c17b95fcd5 // indirect
+	github.com/baiyubin/aliyun-sts-go-sdk v0.0.0-20180326062324-cfa1a18b161f // indirect
+	github.com/caddyserver/certmagic v0.11.2
+	github.com/go-ini/ini v1.44.0 // indirect
+	github.com/hashicorp/golang-lru v0.5.3 // indirect
+	github.com/satori/go.uuid v1.2.0 // indirect
+	github.com/transip/gotransip v0.0.0-20190812104329-6d8d9179b66f // indirect
+)
---
+++ main.go
@@ -0,0 +1,66 @@
+/*
+Web server to be used for serving a single mix update folder.
+Will automatically get a lets encrypt certificate, and use
+that to provide https transport for the domain.
+*/
+
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+
+	"github.com/caddyserver/certmagic"
+)
+
+func main() {
+	dir := flag.String("path", "./", "enter the full path of the directory to serve via https")
+	prod := flag.Bool("prod", false, "set to true if you want a real and signed certificate, and are done with testing")
+	url := flag.String("url", "", "Enter the url for your domain")
+	mail := flag.String("mail", "", "The mail address to use when registering domain for letsencrypt cert")
+
+	flag.Parse()
+
+	//create a file server, and serve the files found in ./
+	fd := http.FileServer(http.Dir(*dir))
+	http.Handle("/", fd)
+
+	// Get certificates and start https
+	certmagic.DefaultACME.Agreed = true
+	certmagic.DefaultACME.Email = "postmannen@gmail.com"
+
+	switch *url {
+	case "":
+		log.Println("You need to specify a domain using the -url flag")
+		return
+	}
+
+	switch *mail {
+	case "":
+		log.Println("You need to specify a mail address using the -mail flag")
+		return
+	}
+
+	switch *prod {
+	case true:
+		certmagic.DefaultACME.CA = certmagic.LetsEncryptProductionCA
+	case false:
+		certmagic.DefaultACME.CA = certmagic.LetsEncryptStagingCA
+	default:
+		log.Println("Info: you need to set the prod flag to true or false")
+		return
+	}
+
+	if *prod {
+		certmagic.DefaultACME.CA = certmagic.LetsEncryptProductionCA
+	} else {
+		certmagic.DefaultACME.CA = certmagic.LetsEncryptStagingCA
+	}
+
+	err := certmagic.HTTPS([]string{*url}, nil)
+	if err != nil {
+		log.Println("--- error: certmagic.HTTPS failed: ", err)
+		return
+	}
+}
